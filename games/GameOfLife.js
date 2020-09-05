6ce49475180e05fc5f544d17d2f6d17a5700efa2
Anton Keks <anton@codeborne.com>
2020-09-06 00:40:14 +0300

    fix game of life: next state is calculated based on unmodified previous state

--- games/GameOfLife.js
+++ games/GameOfLife.js
@@ -2,40 +2,41 @@ import Game from '../core/Game.js'
 import {v} from '../core/Vector.js'
 
 export default class GameOfLife extends Game {
-  live = this.randomPositions(100)
+  alive = this.randomPositions(100)
 
   mouse = addEventListener('click', e => {
     if (!e.target.pos) return
-    this.live.push(e.target.pos)
+    this.alive.push(e.target.pos)
   })
 
   gameTurn() {
+    const next = []
     for (let y = 0; y < this.board.rows; y++) {
       for (let x = 0; x < this.board.cols; x++) {
         const c = v(x, y)
-        const a = this.liveIndex(c)
+        const a = this.isAlive(c)
         const n = this.numNeighbors(c)
-        if (a >= 0 && (n < 2 || n > 3)) this.live.splice(a, 1)
-        if (a < 0 && n === 3) this.live.push(c)
+        if (a && (n === 2 || n === 3) || !a && n === 3) next.push(c)
       }
     }
+    this.alive = next
 
     this.drawLiveCells()
   }
 
-  liveIndex(c) {
+  isAlive(c) {
     c = this.board.wrapBounds(c)
-    return this.live.findIndex(p => p.equals(c))
+    return this.alive.find(p => p.equals(c))
   }
   
   numNeighbors(c) {
-    const n = (x, y) => this.liveIndex(v(x, y)) >= 0 ? 1 : 0
+    const n = (x, y) => this.isAlive(v(x, y)) ? 1 : 0
     return n(c.x - 1, c.y - 1) + n(c.x, c.y - 1) + n(c.x + 1, c.y - 1) +
            n(c.x - 1, c.y) + n(c.x + 1, c.y) +
            n(c.x - 1, c.y + 1) + n(c.x, c.y + 1) + n(c.x + 1, c.y + 1)
   }
 
   drawLiveCells() {
-    this.live.forEach(c => this.board.set(c, 'filled'))
+    this.alive.forEach(c => this.board.set(c, 'filled'))
   }
 }
