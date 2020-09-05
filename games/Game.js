acc43e39f1491a9995c5908a771e62c27116f4c5
Anton Keks <anton@codeborne.com>
2020-09-05 20:12:00 +0300

    use Array.from

--- games/Game.js
+++ games/Game.js
@@ -2,12 +2,12 @@ import {v} from '../Vector.js'
 const {random, floor} = Math
 
 export default class Game {
+  score = 0
+
   constructor(board, speed = 80) {
     this.board = board
     this.speed = speed
   }
-
-  score = 0
 
   commonKeys = addEventListener('keydown', e => {
     switch (e.code) {
@@ -34,15 +34,14 @@ export default class Game {
   }
 
   randomPositions(n) {
-    return Array(n).fill({}).map(() =>
-      (v(floor(random() * this.board.cols), floor(random() * this.board.rows))))
+    return Array.from(Array(n), () => v(floor(random() * this.board.cols), floor(random() * this.board.rows)))
   }
 
   wrapBounds(p) {
     if (p.x > this.board.cols) p.x = 0
-    if (p.x < 0) p.x = this.board.cols
+    else if (p.x < 0) p.x = this.board.cols
     if (p.y > this.board.rows) p.y = 0
-    if (p.y < 0) p.y = this.board.rows
+    else if (p.y < 0) p.y = this.board.rows
     return p
   }
 }
